d16ed3479c8132b7759caf529ef2a5efb260f760
kaisa <kaisa@localhost>
2020-03-21 23:44:27 +0800

    Socket sample

---
+++ Socket/src/Client.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket sock = new Socket("localhost",6666);
+        try(InputStream input = sock.getInputStream()) {
+            try (OutputStream output = sock.getOutputStream()) {
+                handle(input, output);
+            }
+
+        }
+        sock.close();
+        System.out.println("disconnected");
+    }
+
+    private static void handle(InputStream input, OutputStream output) throws IOException {
+        var writer = new BufferedWriter(new OutputStreamWriter(output, StandardCharsets.UTF_8));
+        var reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("[Server] " + reader.readLine());
+        for (; ; ) {
+            System.out.println(">>>");
+            String s = scanner.nextLine();
+            writer.write(s);
+            writer.newLine();
+            writer.flush();
+
+            String resp = reader.readLine();
+            System.out.println("<<< " + resp);
+            if (resp.equals("bye")) {
+                break;
+            }
+        }
+    }
+}
+
